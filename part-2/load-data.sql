49b9cd47986dba4509d8300ba30cec625126a4ac
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-06-19 14:24:50 -0700

    Created the db and added seed data from csv file it db

---
+++ part-2/load-data.sql
@@ -0,0 +1,3 @@
+\c grocery_store
+
+COPY grocery_items (name,price,section) FROM '/Users/michaelmasterson/Desktop/LearnersGuild/phase-3-challenge/part-2/grocery.csv' DELIMITER ',' HEADER CSV;
---
+++ part-2/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS grocery_store;
+CREATE DATABASE grocery_store;
+
+\c grocery_store
+
+DROP TABLE IF EXISTS grocery_items;
+CREATE TABLE grocery_items(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	price DECIMAL NOT NULL,
+	section VARCHAR(40) NOT NULL
+);
+
+DROP TABLE IF EXISTS shoppers;
+CREATE TABLE shoppers(
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(40) NOT NULL,
+	last_name VARCHAR(40) NOT NULL
+);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	section VARCHAR(40) NOT NULL
+);
+
