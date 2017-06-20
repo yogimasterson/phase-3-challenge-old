696db2c0aef860452cead13aa8ce04ede3d3c2a1
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-06-20 12:56:53 -0700

    refactored code a bit

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,46 +1,54 @@
-const cart = []
+const shoppingCart = []
+const cart_button = document.getElementById("cart-button")
+const onion_button = document.getElementById("onion-button")
+const tomato_button = document.getElementById("tomato-button")
+const carrot_button = document.getElementById("carrot-button")
+const cold_cut_button = document.getElementById("cold-cut-button")
+const orange_juice_button = document.getElementById("orange-juice-button")
+const ice_cream_button = document.getElementById("ice-cream-button")
+const pizza_button = document.getElementById("pizza-button")
 
-if (!cart) {
+if (!shoppingCart) {
 	document.getElementById('cart-item-count').innerHTML = '0'
 } else {
-	document.getElementById('cart-item-count').innerHTML = cart.length
+	document.getElementById('cart-item-count').innerHTML = shoppingCart.length
 }
 
-document.getElementById("cart-button").addEventListener("click", function() {
-	console.log(cart);
+cart_button.addEventListener("click", function() {
+	console.log(shoppingCart);
 })
 
-document.getElementById("onion").addEventListener("click", function() {
-  cart.push("Onion");
-	document.getElementById('cart-item-count').innerHTML = cart.length
+onion_button.addEventListener("click", function() {
+  shoppingCart.push("Onion");
+	document.getElementById('cart-item-count').innerHTML = shoppingCart.length
 });
 
-document.getElementById("tomato").addEventListener("click", function() {
-  cart.push("Tomato");
-	document.getElementById('cart-item-count').innerHTML = cart.length
+tomato_button.addEventListener("click", function() {
+  shoppingCart.push("Tomato");
+	document.getElementById('cart-item-count').innerHTML = shoppingCart.length
 });
 
-document.getElementById("carrot").addEventListener("click", function() {
-  cart.push("Carrots");
-	document.getElementById('cart-item-count').innerHTML = cart.length
+carrot_button.addEventListener("click", function() {
+  shoppingCart.push("Carrots");
+	document.getElementById('cart-item-count').innerHTML = shoppingCart.length
 });
 
-document.getElementById("coldCuts").addEventListener("click", function() {
-  cart.push("Cold Cuts");
-	document.getElementById('cart-item-count').innerHTML = cart.length
+cold_cut_button.addEventListener("click", function() {
+  shoppingCart.push("Cold Cuts");
+	document.getElementById('cart-item-count').innerHTML = shoppingCart.length
 });
 
-document.getElementById("orangeJuice").addEventListener("click", function() {
-  cart.push("Orange Juice");
-	document.getElementById('cart-item-count').innerHTML = cart.length
+orange_juice_button.addEventListener("click", function() {
+  shoppingCart.push("Orange Juice");
+	document.getElementById('cart-item-count').innerHTML = shoppingCart.length
 });
 
-document.getElementById("iceCream").addEventListener("click", function() {
-  cart.push("Ice Cream");
-	document.getElementById('cart-item-count').innerHTML = cart.length
+ice_cream_button.addEventListener("click", function() {
+  shoppingCart.push("Ice Cream");
+	document.getElementById('cart-item-count').innerHTML = shoppingCart.length
 });
 
-document.getElementById("pizza").addEventListener("click", function() {
-  cart.push("Pizza");
-	document.getElementById('cart-item-count').innerHTML = cart.length
+pizza_button.addEventListener("click", function() {
+  shoppingCart.push("Pizza");
+	document.getElementById('cart-item-count').innerHTML = shoppingCart.length
 });
